a46c71ecc5a4752a0c4f7dea2ed978c2efb8960e
nmalabaev <nurelmalabaev95@gmail.com>
2026-04-29 10:45:15 +0600

    host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    allowedHosts: "all"
-  },
   preview: {
-    allowedHosts: "all"
+    allowedHosts: [
+      "voting-system-front-production.up.railway.app"
+    ]
   }
 });
